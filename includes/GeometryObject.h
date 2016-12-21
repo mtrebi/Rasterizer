f6a90e9183607ad330fd58f21cab47893c91b3d4
Mtrebi <marianotrebino@gmail.com>
2016-12-21 21:32:04 +0100

    Added vertices and indices attributes

--- includes/GeometryObject.h
+++ includes/GeometryObject.h
@@ -2,15 +2,23 @@
 #define GEOMETRYOBJECT_H
 
 #include "Point3D.h"
+#include "Point2D.h"
+#include <vector>
+#include <cstdint>
 
 class GeometryObject {
-protected:
-    Point3D m_position;
+private:
+	std::vector<Point3D> m_vertices;
+	std::vector<uint8_t> m_indices;
+	// TODO: Normals
+	// TODO: Textures
+
 public:
     GeometryObject();
-    GeometryObject(const Point3D position);  
+    GeometryObject(const std::vector<Point3D>& vertices, const std::vector<uint8_t>& m_indices = std::vector<uint8_t>());
     
     // TODO: Some kind of projection
+	const std::vector<Point2D> Project();
     
     ~GeometryObject();
 };
