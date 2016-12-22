b943022943bf3039a0cb468cc7d9f886d9d0d695
maru <marianotrebino@gmail.com>
2016-12-22 13:09:09 +0100

    Render of an empty world using a rasterizer renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
+
+#include "World.h"
+#include "Renderer.h"
+#include "Rasterizer.h"
+#include "Camera.h"
 
 int main (){
-	std::cout << "Hello world" << std::endl;
+    
+    std::vector<GeometryObject> objects = std::vector<GeometryObject>() {
+        
+    };
+    
+    std::vector<GeometryObject> lights = std::vector<GeometryObject>() {
+        
+    };
+    
+    Camera camera = Camera(Point3D(.0f, .0f, -10.0f), Vector3D(.0f, .0f, 1.0f));
+    
+    World * world = new World(objects, lights, camera);
+    Renderer * renderer = new Rasterizer(world);
+    renderer->render(640, 480, "output.bmp");
 
-	return 0;
+    return 0;
 }
