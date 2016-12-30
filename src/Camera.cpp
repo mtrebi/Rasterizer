481c607b0e03133baed985f494ec2ca94842dd44
Mtrebi <marianotrebino@gmail.com>
2016-12-30 13:16:21 +0100

    Fixed syntax error

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -4,7 +4,7 @@ Camera::Camera()
 	: m_position(Point3D()) {
 }
 
-Camera::    Camera(const Point3D& position, const Vector3D& forward, const Vector3D& up);
+Camera::Camera(const Point3D& position, const Vector3D& forward, const Vector3D& up)
 	: m_position(position), m_forward(forward), m_up(up), m_left(up^forward) {
 
 }
