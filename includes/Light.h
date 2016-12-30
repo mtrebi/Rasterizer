f7727be0d93ee23f990cf67ce8cd83d147a183bc
Mtrebi <marianotrebino@gmail.com>
2016-12-30 13:27:23 +0100

    Created light class (placeholder)

---
+++ includes/Light.h
@@ -0,0 +1,14 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+class Light {
+private:
+
+public:
+  Light();
+  ~Light();
+};
+
+
+#endif /* LIGHT_H */
+
---
+++ src/Light.cpp
@@ -0,0 +1,9 @@
+#include "Light.h"
+
+Light::Light() {
+
+}
+
+Light::~Light() {
+
+}
