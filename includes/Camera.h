ba44e49ffe13c3f979a250ac02fa83352df2b53b
Mtrebi <marianotrebino@gmail.com>
2017-01-04 18:18:15 +0100

    Added projectToScreen virtual function

--- includes/Camera.h
+++ includes/Camera.h
@@ -1,21 +1,24 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include "Point2D.h"
 #include "Point3D.h"
 #include "Vector3D.h"
+#include "Triangle.h"
 
 class Camera {
-private:
+protected:
 	Point3D m_position;
 	Vector3D m_forward,
 			m_up,
 			m_left;
-
+  //TODO far plane, near planer
 public:
     Camera();
     Camera(const Point3D& position, const Vector3D& forward = Vector3D(.0f, .0f, 1.0f), const Vector3D& up = Vector3D(.0f, 1.0f, .0f));
     ~Camera();
-    //TODO
+    
+    virtual const Triangle2D projectToScreen(const Triangle3D& triangle3D) const = 0;
 };
 
 #endif /* CAMERA_H */
