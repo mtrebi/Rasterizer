c6dc6c9c8d88978db3be30de9ec0b88f6d9b7ae1
Mtrebi <marianotrebino@gmail.com>
2017-01-19 12:20:55 +0100

    Fixed bug that related with pixels fill constructor

--- includes/Renderer.h
+++ includes/Renderer.h
@@ -8,6 +8,8 @@
 #include "RGBColor.h"
 #include "World.h"
 
+const RGBColor BACKGROUND_COLOR = RGBColor(1.0f, 1.0f, 1.0f);
+
 class Renderer {
 protected:
   World* m_world;
@@ -15,13 +17,12 @@ protected:
   uint16_t m_image_width,
             m_image_height;
 
-  const RGBColor BACKGROUND_COLOR = RGBColor(1.0f, 1.0f, 1.0f);
 public:
   Renderer();
   Renderer(World* world, const uint16_t image_width, const uint16_t image_height);
   ~Renderer();
     
-  virtual void render(const std::string output_path) const = 0;
+  virtual void render(const std::string output_path) = 0;
 protected:
   void exportImage(const std::string export_path) const;
 };
