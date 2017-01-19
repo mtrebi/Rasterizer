f09a853d5c40cab0a1f138d5f25707a6b6056a75
Mtrebi <marianotrebino@gmail.com>
2017-01-19 12:21:58 +0100

    Promoted Triangle2D from struct to class, removed intersect method and implemented barycentric coordinates based on dot product (instead cross)

--- includes/Triangle.h
+++ includes/Triangle.h
@@ -3,17 +3,59 @@
 
 #include "Point2D.h"
 #include "Point3D.h"
+#include <array>
 
-struct Triangle2D {
+class Triangle2D {
+public:
   Point2D v1, v2, v3;
+public:
+  Triangle2D() { }
+  Triangle2D(const Point2D& p1, const Point2D& p2, const Point2D& p3) : v1(p1), v2(p2), v3(p3) { }
+  ~Triangle2D() { }
 
-  const bool contains(const Point2D point) const {
-    const float area = 0.5 *(-v2.y*v3.x + v1.y*(-v2.x + v3.x) + v1.x*(v2.y - v3.y) + v2.x*v3.y);
+  const bool contains(const Point2D& point) const {
+    float u, v, w;
+    calculateBarycentricCoords(u, v, w, point);
+    return (u >= 0 && v >= 0 && u + v < 1);
+  }
 
-    const float s = 1 / (2 * area)*(v1.y*v3.x - v1.x*v3.y + (v3.y - v1.y)*point.x + (v1.x - v3.x)*point.y);
-    const float t = 1 / (2 * area)*(v1.x*v2.y - v1.y*v2.x + (v1.y - v2.y)*point.x + (v2.x - v1.x)*point.y);
+  void calculateBarycentricCoords(float& u, float& v, float& w, const Point2D& point) const {
+    const Vector2D v0 = this->v2 - this->v1, v1 = this->v3 - this->v1, v2 = point - this->v1;
+    float d00 = v0 * v0;
+    float d01 = v0 * v1;
+    float d11 = v1 * v1;
+    float d20 = v2 * v0;
+    float d21 = v2 * v1;
+    float denom = d00 * d11 - d01 * d01;
+    v = (d11 * d20 - d01 * d21) / denom;
+    w = (d00 * d21 - d01 * d20) / denom;
+    u = 1.0f - v - w;
+  }
 
-    return (s > 0 && t > 0 && 1 - s - t > 0);
+  void calculateBBox(Point2D& bbox_min, Point2D& bbox_max) const {
+    bbox_min.x = std::numeric_limits<float>::max();
+    bbox_min.y = std::numeric_limits<float>::max();
+
+    bbox_max.x = std::numeric_limits<float>::min();
+    bbox_max.y = std::numeric_limits<float>::min();
+
+    std::array<Point2D, 3> vertices{ this->v1, this->v2, this->v3 };
+
+    for (auto& vertex : vertices) {
+      if (vertex.x < bbox_min.x) {
+        bbox_min.x = vertex.x;
+      }
+      if (vertex.y < bbox_min.y) {
+        bbox_min.y = vertex.y;
+      }
+
+      if (vertex.x > bbox_max.x) {
+        bbox_max.x = vertex.x;
+      }
+      if (vertex.y > bbox_max.y) {
+        bbox_max.y = vertex.y;
+      }
+    }
   }
 
 };
@@ -29,38 +71,20 @@ public:
   const bool contains(const Point3D& point) const {
     float u, v, w;
     calculateBarycentricCoords(u, v, w, point);
-    return (u > 0 && v > 0 && u + v <= 0);
+    return (u >= 0 && v >= 0 && u + v < 1);
   }
 
-  const Point3D intersect(bool& hit, const Point3D& ray_origin, const Vector3D& ray_direction) const {
-    Vector3D normal_triangle = ((this->v2 - this->v1) ^ (this->v3 - this->v1));
-    normal_triangle.normalize();
-
-    Vector3D ray_direction_norm = ray_direction;
-    ray_direction_norm.normalize();
-
-    const double t = (this->v1 - ray_origin) * normal_triangle / (ray_direction_norm * normal_triangle);
-    const Point3D intersecion_ray_plane = ray_origin + t * ray_direction_norm;
-    if (this->contains(intersecion_ray_plane)) {
-      hit = true;
-      return intersecion_ray_plane;
-    }
-
-    hit = false;
-    return Point3D();
-  }
-
-private:
   void calculateBarycentricCoords(float& u, float& v, float& w, const Point3D& point) const {
-    const float area_triangle = ((this->v2 - this->v1) ^ (this->v3 - this->v1)).length() / 2;
-
-    const float area_u = ((point - this->v1) ^ (this->v2 - this->v1)).length() / 2;
-    const float area_v = ((point - this->v1) ^ (this->v3 - this->v1)).length() / 2;
-    const float area_w = ((point - this->v3) ^ (this->v3 - this->v2)).length() / 2;
-
-    u = area_u / area_triangle;
-    v = area_v / area_triangle;
-    w = area_w / area_triangle;
+    const Vector3D v0 = this->v2 - this->v1, v1 = this->v3 - this->v1, v2 = point - this->v1;
+    float d00 = v0 * v0;
+    float d01 = v0 * v1;
+    float d11 = v1 * v1;
+    float d20 = v2 * v0;
+    float d21 = v2 * v1;
+    float denom = d00 * d11 - d01 * d01;
+    v = (d11 * d20 - d01 * d21) / denom;
+    w = (d00 * d21 - d01 * d20) / denom;
+    u = 1.0f - v - w;
   }
 };
 
