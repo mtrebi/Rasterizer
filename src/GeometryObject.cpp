3105902e8c7c038f42fc18c2d22b7e76d8dec4e3
Mtrebi <marianotrebino@gmail.com>
2017-01-19 12:24:31 +0100

    Initialize object color to red for testing purposes

--- src/GeometryObject.cpp
+++ src/GeometryObject.cpp
@@ -1,12 +1,13 @@
 #include "GeometryObject.h"
 
 GeometryObject::GeometryObject() {
+  m_color = RGBColor(1, 0, 0);
 
 }
 
 GeometryObject::GeometryObject(const std::vector<Point3D>& vertices, const std::vector<uint8_t>& indices)
   : m_vertices(vertices), m_indices(indices){
-  
+  m_color = RGBColor(1, 0, 0);
 }
 
 GeometryObject::~GeometryObject() {
@@ -18,9 +19,9 @@ const std::vector<Triangle3D> GeometryObject::tessellate() const {
 
   return std::vector<Triangle3D> {
     Triangle3D(
-      Point3D(-5.0f, 0.0f, 0.0f),
-      Point3D(0.0f, 5.0f, 0.0f),
-      Point3D(5.0f, 0.0f, 0.0f)
+      Point3D(-25.0f, 0.0f, 20.0f),
+      Point3D(0.0f, 25.0f, 20.0f),
+      Point3D(25.0f, 0.0f, 20.0f)
     )
   };
 }
