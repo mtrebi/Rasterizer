f0db4744f85aba2e384cee96d195aab72cec49bd
Mtrebi <marianotrebino@gmail.com>
2017-01-19 19:05:59 +0100

    Implemented function to convert from 2d array indexing to 1d

--- includes/Utils.h
+++ includes/Utils.h
@@ -8,6 +8,10 @@ public:
     pixel_y =  i / m_image_width;
   }
 
+  static uint32_t convert2DIndexto1DIndex(const uint16_t& pixel_x, const uint16_t& pixel_y, const uint16_t m_image_width, const uint16_t m_image_height) {
+    const uint32_t i = pixel_y * m_image_width + pixel_x;
+    return i;
+  }
 };
 #endif // !UTILS_H
 
