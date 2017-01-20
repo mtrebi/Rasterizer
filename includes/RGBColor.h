55cf82394a614d671f65a34dee9d5cfcb7ddddc8
Mtrebi <marianotrebino@gmail.com>
2017-01-20 11:10:50 +0100

    Added plus operator

--- includes/RGBColor.h
+++ includes/RGBColor.h
@@ -10,6 +10,15 @@ public:
   RGBColor(const float red, const float green, const float blue);
 
   //TODO: COlor operations (add, substract...)
+
+  RGBColor operator+(const RGBColor& color) const {
+    RGBColor new_color;
+    new_color.r = this->r + color.r;
+    new_color.g = this->g + color.g;
+    new_color.b = this->b + color.b;
+  }
+
+
   ~RGBColor();
 };
 
