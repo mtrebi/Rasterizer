004f0417a6e69e15e994affcc04ee9f7fafdcc60
Mtrebi <marianotrebino@gmail.com>
2017-01-20 13:08:16 +0100

    Implemented two methods to get color and light direction

--- includes/Light.h
+++ includes/Light.h
@@ -1,12 +1,20 @@
 #ifndef LIGHT_H
 #define LIGHT_H
 
+#include "Point3D.h"
+#include "Vector3D.h"
+#include "RGBColor.h"
+
 class Light {
 private:
-
+  RGBColor m_color;
 public:
   Light();
+  Light(const RGBColor& color);
   ~Light();
+
+  virtual const Vector3D getDirectionToPoint(const Point3D& position) const = 0;
+  const RGBColor& getColor();
 };
 
 
--- src/Light.cpp
+++ src/Light.cpp
@@ -4,6 +4,15 @@ Light::Light() {
 
 }
 
+Light::Light(const RGBColor& color)
+  : m_color(color) {
+
+}
+
 Light::~Light() {
 
+}
+
+const RGBColor & Light::getColor() {
+  return m_color;
 }
