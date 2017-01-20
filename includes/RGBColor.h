36ae24bfabb1dc4c3d364b0ebbadf42b9aaa2f5a
Mtrebi <marianotrebino@gmail.com>
2017-01-20 12:07:47 +0100

    Implemented multiplication operator and fixed bug that caused rgb component to be >1

--- includes/RGBColor.h
+++ includes/RGBColor.h
@@ -1,6 +1,8 @@
 #ifndef RGBCOLOR_H
 #define RGBCOLOR_H
 
+#include <algorithm> //min
+
 class RGBColor  {
 public:
   float r, g, b;
@@ -13,11 +15,17 @@ public:
 
   RGBColor operator+(const RGBColor& color) const {
     RGBColor new_color;
-    new_color.r = this->r + color.r;
-    new_color.g = this->g + color.g;
-    new_color.b = this->b + color.b;
+    new_color.r = std::min(this->r + color.r, 1.0f);
+    new_color.g = std::min(this->g + color.g, 1.0f);
+    new_color.b = std::min(this->b + color.b, 1.0f);
   }
 
+  RGBColor operator*(const RGBColor& color) const {
+    RGBColor new_color;
+    new_color.r = this->r * color.r;
+    new_color.g = this->g * color.g;
+    new_color.b = this->b * color.b;
+  }
 
   ~RGBColor();
 };
