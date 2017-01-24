b676ced3ea127591522b6fda832809326cc8e5ca
Mtrebi <marianotrebino@gmail.com>
2017-01-24 17:37:03 +0100

    Added image dimensions and moved getDepth method to rasterizer

--- includes/Camera.h
+++ includes/Camera.h
@@ -15,17 +15,18 @@ protected:
 			m_up,
 			m_left;
   uint32_t m_near, m_far;
-  float m_zoom;
+  float m_height, 
+    m_width,
+    m_fov;
 public:
     Camera();
-    Camera(const Point3D& position, const Vector3D& forward = Vector3D(.0f, .0f, 1.0f), const Vector3D& up = Vector3D(.0f, 1.0f, .0f), const uint32_t near = 10, const uint32_t far = 1000, const float zoom = 1.0f);
+    Camera(const Point3D& position, const Vector3D& forward = Vector3D(.0f, .0f, 1.0f), const Vector3D& up = Vector3D(.0f, 1.0f, .0f), const uint32_t near = 10, const uint32_t far = 1000, const float fov = 1.0f);
     ~Camera();
       
     virtual const Vector3D viewDirection(const Point3D point) const = 0;
     virtual const Triangle2D worldSpaceToScreenSpace(const Triangle3D& triangle3D) const = 0;
     virtual const Point2D cameraToScreen(const uint16_t image_width, const uint16_t image_height, const Point2D& point_camera) const = 0;
     virtual const Point3D imageSpaceToWorldSpace(const uint16_t image_width, const uint16_t image_height, const uint16_t pixel_image_x, const uint16_t pixel_image_y) const = 0;
-    virtual const float getDepth(const Triangle3D& triangle_world, const Triangle2D& triangle_screen, const Point2D& pixel_screen) const = 0;
 };
 
 #endif /* CAMERA_H */
