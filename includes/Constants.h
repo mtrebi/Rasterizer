8910d67d1b7b67f60dffa10e6cb43fdd1bd18167
Mtrebi <marianotrebino@gmail.com>
2017-01-26 10:27:37 +0100

    Moved constants to constants file

---
+++ includes/Constants.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+
+#include "POINT3D.H"
+#include "VECTOR3D.H"
+#include "Material.h"
+#include "RGBCOLOR.H"
+
+const Point3D   CAMERA_POS = Point3D(.0f, .0f, -1.0f);
+const Vector3D  CAMERA_FWD = Vector3D(.0f, .0f, 1.0f); // Points into the screen
+const Vector3D  CAMERA_UP = Vector3D(.0f, 1.0f, .0f);
+
+const int IMAGE_WIDTH = 500;
+const int IMAGE_HEIGHT = 500;
+const std::string IMAGE_NAME = "output.bmp";
+
+
+namespace Materials {
+  const Material PLASTIC = Material(0.4, 0.4, 2);
+  const Material WALL = Material(0.6, 0.2, 1);
+}
+
+namespace Colors {
+  const RGBColor RED = RGBColor(1.0, 0.0, 0.0);
+  const RGBColor GREEN = RGBColor(0.0, 1.0, 0.0);
+  const RGBColor BLUE = RGBColor(0.0, 0.0, 1.0);
+  const RGBColor GREY = RGBColor(0.5, 0.5, 0.5);
+  const RGBColor YELLOW = RGBColor(1.0, 1.0, 0.0);
+  const RGBColor PURPLE = RGBColor(1.0, 0.0, 1.0);
+  const RGBColor CYAN = RGBColor(0.0, 1.0, 1.0);
+}
+
