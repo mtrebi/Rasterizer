8b1f80c6cfc3fa2a9785f7d41dfa599628c1504a
Mtrebi <marianotrebino@gmail.com>
2017-01-26 10:24:21 +0100

    Deleted test values on constructors

--- src/GeometryObject.cpp
+++ src/GeometryObject.cpp
@@ -1,14 +1,11 @@
 #include "GeometryObject.h"
 #include <cassert>
 GeometryObject::GeometryObject() {
-  m_color = RGBColor(1.0f, 0.1f, 0.1f);
-  m_material = Material(RGBColor(0.4f), RGBColor(0.4f), 1.0f);
+
 }
 
 GeometryObject::GeometryObject(const RGBColor& color, const Material& material, const std::vector<Point3D>& vertices, const std::vector<uint32_t>& indices)
   : m_vertices(vertices), m_indices(indices), m_material(material), m_color(color) {
-  m_color = RGBColor(1.0f, 0.0f, 0.0f);
-
 }
 
 GeometryObject::~GeometryObject() {
