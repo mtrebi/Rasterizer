3c11669a454d927edca7a037bc01ab4459381dc8
Mtrebi <marianotrebino@gmail.com>
2017-01-26 18:43:51 +0100

    Modified image size to 800x600

--- includes/Constants.h
+++ includes/Constants.h
@@ -10,8 +10,8 @@ const Point3D   CAMERA_POS = Point3D(.0f, .0f, -1.0f);
 const Vector3D  CAMERA_FWD = Vector3D(.0f, .0f, 1.0f); // Points into the screen
 const Vector3D  CAMERA_UP = Vector3D(.0f, 1.0f, .0f);
 
-const int IMAGE_WIDTH = 500;
-const int IMAGE_HEIGHT = 500;
+const int IMAGE_WIDTH = 800;
+const int IMAGE_HEIGHT = 600;
 const std::string IMAGE_NAME = "output.bmp";
 
 
