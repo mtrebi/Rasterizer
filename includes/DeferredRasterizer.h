d45324e182c3101b371035e2b956b6ffdbfeaa1b
Mtrebi <marianotrebino@gmail.com>
2017-02-07 10:49:00 +0100

    Implemented deferred shading

--- includes/DeferredRasterizer.h
+++ includes/DeferredRasterizer.h
@@ -2,10 +2,18 @@
 #include "RASTERIZER.H"
 
 class DeferredRasterizer : public Rasterizer {
+private:
+  std::vector<RGBColor> m_normal_buffer;
+  std::vector<RGBColor> m_color_buffer;
+  std::vector<RGBColor> m_diffuse_buffer;
+  std::vector<RGBColor> m_specular_buffer;
 public:
   DeferredRasterizer();
   DeferredRasterizer(World* world);
   ~DeferredRasterizer();
 
   void render(const std::string output_path, const uint16_t image_width, const uint16_t image_height) override;
+
+private:
+
 };
--- src/DeferredRasterizer.cpp
+++ src/DeferredRasterizer.cpp
@@ -15,7 +15,13 @@ DeferredRasterizer::~DeferredRasterizer() {
 void DeferredRasterizer::render(const std::string output_path, const uint16_t image_width, const uint16_t image_height) {
   m_pixels = std::vector<RGBColor>(image_height * image_width, BACKGROUND_COLOR);
   m_depth_buffer = std::vector<double>(image_height * image_width, m_camera->get_far_plane());
+  m_normal_buffer = std::vector<RGBColor>(image_height * image_width, BACKGROUND_COLOR);
+  m_color_buffer = std::vector<RGBColor>(image_height * image_width, BACKGROUND_COLOR);
+  m_diffuse_buffer = std::vector<RGBColor>(image_height * image_width, BACKGROUND_COLOR);
+  m_specular_buffer = std::vector<RGBColor>(image_height * image_width, BACKGROUND_COLOR);
 
+
+  // Render color, depth and normals
   for (auto& object : m_world->m_objects) {
     const std::vector<Triangle3D> triangles = object->triangles();
     for (auto& triangle_world : triangles) {
@@ -33,9 +39,14 @@ void DeferredRasterizer::render(const std::string output_path, const uint16_t im
             if (m_camera->insideFrustrum(pixel_raster, depth)) {
               const uint32_t i = pixel_raster_y * image_width + pixel_raster_x;
               if (depth < m_depth_buffer[i]) {
-                const Vertex3D vertex = calculateVertexAttributes(triangle_world, pixel_world);
-                m_pixels[i] = object->material()->shade(m_world->m_lights, *m_world->m_camera, vertex);
+                const Fragment fragment = calculateFragmentAttributes(triangle_world, pixel_world, *object->material());
+
+                //TODO: Use one-single buffer of fragment objects
                 m_depth_buffer[i] = depth;
+                m_normal_buffer[i] = (RGBColor) fragment.normal;
+                m_color_buffer[i] = fragment.color;
+                m_diffuse_buffer[i] = fragment.diffuse;
+                m_specular_buffer[i] = fragment.specular;
               }
             }
           }
@@ -44,6 +55,36 @@ void DeferredRasterizer::render(const std::string output_path, const uint16_t im
     }
   }
 
-  exportDepthBuffer(m_depth_buffer, "depth.bmp", image_width, image_height);
-  exportImage(m_pixels, output_path, image_width, image_height);
+  // Calculate lighting only ONCE in screen space
+  for (int x = 0; x < image_width; ++x) {
+    for (int y = 0; y < image_height; ++y) {
+      const uint32_t i = y * image_width + x;
+      const double depth = m_depth_buffer[i];
+      if (depth < m_camera->get_far_plane()) {
+        const Point2D pixel_raster(x, y);
+        const Point2D pixel_projected = this->unproject(pixel_raster);
+        const Point3D pixel_view = m_camera->projectTransformInv(pixel_projected, m_depth_buffer[i]);
+        const Point3D pixel_world = m_camera->viewTransformInv(pixel_view);
+
+        Fragment fragment{
+          pixel_world,
+          m_color_buffer[i],
+          m_diffuse_buffer[i],
+          m_specular_buffer[i],
+          (Vector3D)m_normal_buffer[i]
+        };
+        m_pixels[i] = Material::shade(m_world->m_lights, *m_world->m_camera, fragment);
+      }
+      else {
+        m_pixels[i] = BACKGROUND_COLOR;
+      }
+    }
+  }
+
+  exportDepthBuffer(m_depth_buffer, "d_depth.bmp", image_width, image_height);
+  exportImage(m_normal_buffer, "d_normals.bmp", image_width, image_height);
+  exportImage(m_color_buffer, "d_colors.bmp", image_width, image_height);
+  exportImage(m_diffuse_buffer, "d_diffuse.bmp", image_width, image_height);
+  exportImage(m_specular_buffer, "d_specular.bmp", image_width, image_height);
+  exportImage(m_pixels, "d_" + output_path, image_width, image_height);
 }
