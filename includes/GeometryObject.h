ebcc519f781b152fa567f643fdf03f06271eff08
Mtrebi <marianotrebino@gmail.com>
2017-02-07 14:55:44 +0100

    Refactor geometry object to store all input data (vertices, indices, colors and texture coordinates) in order to implement rotation

--- includes/GeometryObject.h
+++ includes/GeometryObject.h
@@ -11,19 +11,21 @@ class Material;
 
 class GeometryObject {
 private:
-	std::vector<Triangle3D> m_triangles;
 	Material *m_material;
-
+  std::vector<Point3D> m_vertices;
+  std::vector<RGBColor> m_colors;
+  std::vector<Vector2D> m_texture_coords;
+  std::vector<uint32_t> m_indices;
 public:
 	GeometryObject();
 	GeometryObject(Material* material, const std::vector<Point3D>& vertices, const std::vector<RGBColor>& colors, const std::vector<Vector2D>& texture_coords, const std::vector<uint32_t>& indices);
 	~GeometryObject();
 
+  void rotate(const float roll, const float pitch, const float yaw);
 	const std::vector<Triangle3D> triangles() const;
 	const Material * material() const;
 	
 private:
-	const std::vector<Triangle3D> triangulate(const std::vector<Point3D>& vertices, const std::vector<RGBColor>& colors, const std::vector<Vector2D>& texture_coords, const std::vector<uint32_t>& indices) const;
 	const Vertex3D build_vertex(const std::vector<Point3D>& vertices, const std::vector<RGBColor>& colors, const std::vector<Vector2D>& texture_coords, const uint32_t vertex_index) const;
 };
 
--- src/GeometryObject.cpp
+++ src/GeometryObject.cpp
@@ -6,34 +6,12 @@ GeometryObject::GeometryObject() {
 }
 
 GeometryObject::GeometryObject(Material* material, const std::vector<Point3D>& vertices, const std::vector<RGBColor>& colors, const std::vector<Vector2D>& texture_coords, const std::vector<uint32_t>& indices)
-  : m_material(material), m_triangles(triangulate(vertices, colors, texture_coords, indices)) {
+  : m_material(material), m_vertices(vertices), m_colors(colors), m_texture_coords(texture_coords), m_indices(indices) {
   
 }
 
 GeometryObject::~GeometryObject() {
 
-}
-
-const std::vector<Triangle3D> GeometryObject::triangulate(const std::vector<Point3D>& vertices, const std::vector<RGBColor>& colors, const std::vector<Vector2D>& texture_coords, const std::vector<uint32_t>& indices) const {
-  assert(indices.size() % 3 == 0 && "Indices array must be made up of triangles");
-  assert((colors.size() == 0 || vertices.size() == colors.size()) && "Vertices size and colors size should match");
-  assert((texture_coords.size() == 0 || vertices.size() == texture_coords.size()) && "Vertices size and texture coordinates size should match");
-
-  std::vector<Triangle3D> triangles;
-  for (int i = 0; i < indices.size(); i+=3) {
-    Triangle3D triangle = Triangle3D(
-      build_vertex(vertices, colors, texture_coords, indices[i]),
-      build_vertex(vertices, colors, texture_coords, indices[i + 1]),
-      build_vertex(vertices, colors, texture_coords, indices[i + 2])
-    );
-    triangle.v1.normal = triangle.normal;
-    triangle.v2.normal = triangle.normal;
-    triangle.v3.normal = triangle.normal;
-
-    triangles.push_back(triangle);
-  }
-  
-  return triangles;
 }
 
 const Vertex3D GeometryObject::build_vertex(const std::vector<Point3D>& vertices, const std::vector<RGBColor>& colors, const std::vector<Vector2D>& texture_coords, const uint32_t vertex_index) const {
@@ -46,8 +24,32 @@ const Vertex3D GeometryObject::build_vertex(const std::vector<Point3D>& vertices
   return v;
 }
 
+void GeometryObject::rotate(const float roll, const float pitch, const float yaw) {
+  for (auto& point : m_vertices) {
+    //Apply rotation
+  }
+}
+
 const std::vector<Triangle3D> GeometryObject::triangles() const { 
-  return m_triangles; 
+  assert(m_indices.size() % 3 == 0 && "Indices array must be made up of triangles");
+  assert((m_colors.size() == 0 || m_vertices.size() == m_colors.size()) && "Vertices size and colors size should match");
+  assert((m_texture_coords.size() == 0 || m_vertices.size() == m_texture_coords.size()) && "Vertices size and texture coordinates size should match");
+
+  std::vector<Triangle3D> triangles;
+  for (int i = 0; i < m_indices.size(); i += 3) {
+    Triangle3D triangle = Triangle3D(
+      build_vertex(m_vertices, m_colors, m_texture_coords, m_indices[i]),
+      build_vertex(m_vertices, m_colors, m_texture_coords, m_indices[i + 1]),
+      build_vertex(m_vertices, m_colors, m_texture_coords, m_indices[i + 2])
+    );
+    triangle.v1.normal = triangle.normal;
+    triangle.v2.normal = triangle.normal;
+    triangle.v3.normal = triangle.normal;
+
+    triangles.push_back(triangle);
+  }
+
+  return triangles;
 }
 const Material* GeometryObject::material() const { 
   return m_material; 
