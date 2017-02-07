32d86b5591101479b9b80703f9084a5633e344d6
Mtrebi <marianotrebino@gmail.com>
2017-02-07 10:35:48 +0100

    Replaced instance function call shade by static funcion call Material::shade

--- src/ForwardRasterizer.cpp
+++ src/ForwardRasterizer.cpp
@@ -34,7 +34,7 @@ void ForwardRasterizer::render(const std::string output_path, const uint16_t ima
               const uint32_t i = pixel_raster_y * image_width + pixel_raster_x;
               if (depth < m_depth_buffer[i]) {
                 const Fragment fragment = calculateFragmentAttributes(triangle_world, pixel_world, *object->material());
-                m_pixels[i] = object->material()->shade(m_world->m_lights, *m_world->m_camera, fragment);
+                m_pixels[i] = Material::shade(m_world->m_lights, *m_world->m_camera, fragment);
                 m_depth_buffer[i] = depth;
               }
             }
