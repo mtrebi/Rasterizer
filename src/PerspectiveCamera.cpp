b9f294741c9d8d610f2ad02422f1abebde90f5d6
Mtrebi <marianotrebino@gmail.com>
2017-02-13 20:14:06 +0100

    Added formula to normalize between [-1,1] in perspective divide

--- src/PerspectiveCamera.cpp
+++ src/PerspectiveCamera.cpp
@@ -24,18 +24,24 @@ const Vector3D PerspectiveCamera::viewDirection(const Point3D& point) const {
 // TODO: generic
 const Point2D PerspectiveCamera::projectTransform(const Point3D& point_camera) const {
   // Perspective divide
+  const double z_norm = ((m_far + m_near) / (m_far - m_near)) + 
+                      (1 / point_camera.z) * ((2.0 * m_near*m_far) / (m_far - m_near));
+
   const Point2D point_projected = {
-    (point_camera.x * m_near * m_zoom) / (point_camera.z * get_aspect()),
-    (point_camera.y * m_near * m_zoom) / (point_camera.z * get_aspect())
+    (point_camera.x  * m_zoom) / (z_norm ),
+    (point_camera.y  * m_zoom) / (z_norm )
   };
 
   return point_projected;
 }
 
 const Point3D PerspectiveCamera::projectTransformInv(const Point2D& point_projected, const double depth) const {
+  const double z_norm = ((m_far + m_near) / (m_far - m_near)) + 
+                      (1 / depth) * ((2 * m_near*m_far) / (m_far - m_near));
+
   const Point3D point_world = {
-    (point_projected.x * depth * get_aspect()) / (m_near * m_zoom),
-    (point_projected.y * depth * get_aspect()) / (m_near * m_zoom),
+    (point_projected.x * z_norm ) / (m_zoom),
+    (point_projected.y * z_norm ) / (m_zoom),
     depth
   };
 
