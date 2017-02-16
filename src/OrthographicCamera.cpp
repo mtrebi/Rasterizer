0d9f7c91007b34e88013ec414e571e3101a0026f
Mtrebi <marianotrebino@gmail.com>
2017-02-16 10:34:43 +0100

    Removed m_near in orthographic projection

--- src/OrthographicCamera.cpp
+++ src/OrthographicCamera.cpp
@@ -16,8 +16,8 @@ OrthographicCamera::~OrthographicCamera() {
 
 const Point2D OrthographicCamera::projectTransform(const Point3D& point_camera) const {
   const Point2D point_projected = {
-    point_camera.x * m_near * get_aspect(),
-    point_camera.y * m_near * get_aspect(),
+    (point_camera.x * get_aspect()),
+    (point_camera.y * get_aspect()),
   };
 
   return point_projected;
@@ -26,8 +26,8 @@ const Point2D OrthographicCamera::projectTransform(const Point3D& point_camera) 
 
 const Point3D OrthographicCamera::projectTransformInv(const Point2D& pixel_projected, const double depth) const {
   const Point3D pixel_world = {
-    (pixel_projected.x) / (m_near * get_aspect()),
-    (pixel_projected.y) / (m_near * get_aspect()),
+    (pixel_projected.x ) / (get_aspect()),
+    (pixel_projected.y ) / (get_aspect()),
     depth
   };
 
