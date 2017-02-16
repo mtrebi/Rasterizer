1d974bc05bfcc20c6ed20e48c1e21f0d4bc7cc98
Mtrebi <marianotrebino@gmail.com>
2017-02-16 10:33:04 +0100

    Added aspect ratio factor to perspective divide

--- src/PerspectiveCamera.cpp
+++ src/PerspectiveCamera.cpp
@@ -21,27 +21,26 @@ const Vector3D PerspectiveCamera::viewDirection(const Point3D& point) const {
   return view;
 }
 
-// TODO: generic
 const Point2D PerspectiveCamera::projectTransform(const Point3D& point_camera) const {
   // Perspective divide
-  const double z_norm = ((m_far + m_near) / (m_far - m_near)) + 
+  const double w = ((m_far + m_near) / (m_far - m_near)) + 
                       (1 / point_camera.z) * ((2.0 * m_near*m_far) / (m_far - m_near));
 
   const Point2D point_projected = {
-    (point_camera.x  * m_zoom) / (z_norm ),
-    (point_camera.y  * m_zoom) / (z_norm )
+    (point_camera.x  * m_zoom * get_aspect()) / w,
+    (point_camera.y  * m_zoom * get_aspect()) / w
   };
 
   return point_projected;
 }
 
 const Point3D PerspectiveCamera::projectTransformInv(const Point2D& point_projected, const double depth) const {
-  const double z_norm = ((m_far + m_near) / (m_far - m_near)) + 
+  const double w = ((m_far + m_near) / (m_far - m_near)) +
                       (1 / depth) * ((2 * m_near*m_far) / (m_far - m_near));
 
   const Point3D point_world = {
-    (point_projected.x * z_norm ) / (m_zoom),
-    (point_projected.y * z_norm ) / (m_zoom),
+    (point_projected.x * w) / (m_zoom * get_aspect()),
+    (point_projected.y * w) / (m_zoom * get_aspect()),
     depth
   };
 
