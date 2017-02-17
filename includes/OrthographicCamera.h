8f77a9bef1252bf740d6b01cf7b7041fbee1c50c
Mtrebi <marianotrebino@gmail.com>
2017-02-17 11:35:06 +0100

    Fixed orthographic projection matrix

--- includes/OrthographicCamera.h
+++ includes/OrthographicCamera.h
@@ -9,10 +9,9 @@ public:
   OrthographicCamera(const Point3D& position, const Point3D& target, const uint32_t image_height, const uint32_t m_image_width, Renderer * renderer);
   ~OrthographicCamera();
 
-  const Point2D projectTransform(const Point3D& point_camera) const;
-  const Point3D projectTransformInv(const Point2D& pixel_projected, const double depth) const;
+  const Point3D projectTransformInv(const Point2D& point_projected, const double depth) const override;
 
-  const Vector3D viewDirection(const Point3D& point) const;
+  const Vector3D viewDirection(const Point3D& point) const override;
 };
 
 #endif // !ORTHOGRAPHICCAMERA_H
--- src/OrthographicCamera.cpp
+++ src/OrthographicCamera.cpp
@@ -8,14 +8,14 @@ OrthographicCamera::OrthographicCamera()
 OrthographicCamera::OrthographicCamera(const Point3D& position, const Point3D& target, const uint32_t image_height, const uint32_t image_width, Renderer * renderer)
   : Camera(position, target, image_height, image_width, renderer) {
 
-  const float z_range = m_near - m_far;
-  const float tan_half_fov = tan((90 / 2) * 3.1415926535897 / 180);
+  const double z_range = m_far - m_near;
 
-  m_project[0][0] = 1 / get_aspect();                                      m_project[0][1] = 0;                                     m_project[0][2] = 0;                                     m_project[0][3] = 0;
-  m_project[1][0] = 0;                                     m_project[1][1] = 1;                                     m_project[1][2] = 0;                                     m_project[1][3] = 0;
-  m_project[2][0] = 0;                                     m_project[2][1] = 0;                                     m_project[2][2] = 2 / z_range;                   m_project[2][3] = 1;
-  m_project[3][0] = 0;                                     m_project[3][1] = 0;                                     m_project[3][2] = -(m_far + m_near) / z_range;       m_project[3][3] = 0;
+  m_project[0][0] = 2.0 / image_width;                     m_project[0][1] = 0;                       m_project[0][2] = 0;                      m_project[0][3] = 0;
+  m_project[1][0] = 0;                                     m_project[1][1] = 2.0 / image_height;      m_project[1][2] = 0;                      m_project[1][3] = 0;
+  m_project[2][0] = 0;                                     m_project[2][1] = 0;                       m_project[2][2] = 1.0 / z_range;          m_project[2][3] = 0;
+  m_project[3][0] = 0;                                     m_project[3][1] = 0;                       m_project[3][2] = m_near / z_range;       m_project[3][3] = 1;
 
+  m_project = glm::transpose(m_project);
   m_project_inv = glm::inverse(m_project);
 
 }
@@ -23,6 +23,15 @@ OrthographicCamera::OrthographicCamera(const Point3D& position, const Point3D& t
 OrthographicCamera::~OrthographicCamera() {
 
 }
+
+// NDC [-1,1] to Camera space
+const Point3D OrthographicCamera::projectTransformInv(const Point2D& point_projected, const double depth) const {
+  const glm::vec4 point = glm::vec4(point_projected.x, point_projected.y, depth, 1);
+  const glm::vec4 r = point * m_project_inv;
+  return Point3D(r.x, r.y, depth);
+}
+
+
 
 
 const Vector3D OrthographicCamera::viewDirection(const Point3D& point) const {
