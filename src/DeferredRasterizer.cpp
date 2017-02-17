4ef4e2de98baa461a8422c0d8d09946d329008d6
Mtrebi <marianotrebino@gmail.com>
2017-02-17 11:08:52 +0100

    Fixed renderers to use new unrasterize funtion

--- src/DeferredRasterizer.cpp
+++ src/DeferredRasterizer.cpp
@@ -28,21 +28,18 @@ void DeferredRasterizer::render() {
   for (auto& object : m_world->m_objects) {
     const std::vector<Triangle3D> triangles = object->triangles();
     for (auto& triangle_world : triangles) {
-      const Triangle2D triangle_projected = this->project(triangle_world);
       const Triangle2D triangle_raster = this->rasterize(triangle_world);
       const BoundingBox2D bbox_raster = triangle_raster.bbox();
       for (int32_t pixel_raster_x = bbox_raster.min.x; pixel_raster_x <= bbox_raster.max.x; ++pixel_raster_x) {
         for (int32_t pixel_raster_y = bbox_raster.min.y; pixel_raster_y <= bbox_raster.max.y; ++pixel_raster_y) {
           const Point2D pixel_raster = { (double)pixel_raster_x, (double)pixel_raster_y };
           if (triangle_raster.contains(pixel_raster)) {
-            const Point2D pixel_projected = this->unproject(pixel_raster);
-            const double depth = calculateDepth(triangle_world, triangle_projected, pixel_projected);
-            const Point3D pixel_view = m_camera->projectTransformInv(pixel_projected, depth);
-            const Point3D pixel_world = m_camera->viewTransformInv(pixel_view);
+            const double depth = calculateDepth(triangle_world, triangle_raster, pixel_raster);
+            const Point3D pixel_world = unrasterize(pixel_raster, depth);
             if (m_camera->insideFrustrum(pixel_raster, depth)) {
               const uint32_t i = pixel_raster_y * image_width + pixel_raster_x;
               if (depth < m_depth_buffer[i]) {
-                const Fragment fragment = calculateFragmentAttributes(triangle_world, pixel_world, triangle_projected, pixel_projected, *object->material());
+                const Fragment fragment = calculateFragmentAttributes(triangle_world, pixel_world, triangle_raster, pixel_raster, *object->material());
 
                 //TODO: Use one-single buffer of fragment objects
                 m_depth_buffer[i] = depth;
