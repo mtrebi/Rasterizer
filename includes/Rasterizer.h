e9d3ebbf560274b140e2afcc8d187563b204ddb5
Mtrebi <marianotrebino@gmail.com>
2017-02-17 11:10:45 +0100

    Removed old unused unproject functions

--- includes/Rasterizer.h
+++ includes/Rasterizer.h
@@ -33,9 +33,6 @@ protected:
 
   const Point3D unrasterize(const Point2D& point_raster, const double depth_from_camera) const;
 
-  const Triangle2D project(const Triangle3D& triangle_world) const;
-  const Point2D project(const Point3D& point_world) const;
-  const Point2D unproject(const Point2D& point_raster) const;
 
   void exportDepthBuffer(const std::vector<double>& depth_buffer, const std::string output_path, const uint16_t image_width, const uint16_t image_height) const;
 };
