80c64f6f99d1781fed174653788e7d9282baeab5
Mtrebi <marianotrebino@gmail.com>
2017-02-17 11:05:36 +0100

    Fixed project matrix to do a proper normalization of z

--- src/PerspectiveCamera.cpp
+++ src/PerspectiveCamera.cpp
@@ -11,13 +11,15 @@ PerspectiveCamera::PerspectiveCamera(const Point3D& position, const Point3D& tar
   m_zoom = 1 / tan((fov/ 2) * 3.1415926535897 / 180);
 
 
-  const float z_range = m_near - m_far;
   const float tan_half_fov = tan((m_fov / 2) * 3.1415926535897 / 180);
-
+  const double z_range = m_far - m_near;
+  
   m_project[0][0] = 1 / (tan_half_fov * get_aspect());     m_project[0][1] = 0;                                     m_project[0][2] = 0;                                     m_project[0][3] = 0;
   m_project[1][0] = 0;                                     m_project[1][1] = 1 / tan_half_fov;                      m_project[1][2] = 0;                                     m_project[1][3] = 0;
-  m_project[2][0] = 0;                                     m_project[2][1] = 0;                                     m_project[2][2] = (m_far + m_near) / z_range;            m_project[2][3] = 1;
-  m_project[3][0] = 0;                                     m_project[3][1] = 0;                                     m_project[3][2] = (-2 * m_near * m_far) / z_range;       m_project[3][3] = 0;
+  m_project[2][0] = 0;                                     m_project[2][1] = 0;                                     m_project[2][2] = m_far / z_range;                        m_project[2][3] = 1;
+  m_project[3][0] = 0;                                     m_project[3][1] = 0;                                     m_project[3][2] = (m_far * m_near) / -z_range;       m_project[3][3] = 0;
+  
+  m_project = glm::transpose(m_project);
 
   m_project_inv = glm::inverse(m_project);
 }
