e9a57d4e1a133537645525b535cc5186b3b85f7a
Mtrebi <marianotrebino@gmail.com>
2017-03-13 20:06:12 +0100

    Added shadow map size members

--- includes/Light.h
+++ includes/Light.h
@@ -14,11 +14,16 @@ class Light {
 protected:
   RGBColor m_color;
 
+  const int SHADOW_MAP_WIDTH = 800;
+  const int SHADOW_MAP_HEIGHT = 600;
+
 public:
   Light();
   Light(const RGBColor& color);
   ~Light();
 
+
+  virtual const Point3D getPosition() const = 0;
   virtual const Vector3D getDirectionToPoint(const Point3D& position) const = 0;
   virtual const std::vector<double> getShadowMap(const World* world, const int image_height, const int image_width, Rasterizer * rasterizer_camera) const = 0;
 
